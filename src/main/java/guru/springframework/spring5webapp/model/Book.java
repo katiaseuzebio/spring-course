3177f6d304cf3ea7acd9a2500c2a035c46e05d00
Katia Santos Euzebio <katiaseuzebio@gmail.com>
2019-05-20 18:51:48 -0300

    Initial version

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,48 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ *
+ */
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
